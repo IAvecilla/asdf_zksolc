fafa50b2c1619fd87c212659424a4cb5883bdc05
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-06-27 02:09:27 -0300

    Fix format

--- lib/utils.bash
+++ lib/utils.bash
@@ -7,66 +7,66 @@ TOOL_NAME="zksolc"
 TOOL_TEST="zksolc --version"
 case $(uname -s) in
 'Linux')
-	OS="linux-amd64-musl"
-	;;
+    OS="linux-amd64-musl"
+    ;;
 'Darwin')
-	OS="macosx-arm64"
-	;;
+    OS="macosx-arm64"
+    ;;
 esac
 
 fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
-	exit 1
+    echo -e "asdf-$TOOL_NAME: $*"
+    exit 1
 }
 
 curl_opts=(-fsSL)
 
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+        LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" | cut -d/ -f3- | grep -oE '[0-9]+\.[0-9]+\.[0-9]+$'
+    git ls-remote --tags --refs "$GH_REPO" | cut -d/ -f3- | grep -oE '[0-9]+\.[0-9]+\.[0-9]+$'
 }
 
 list_all_versions() {
-	list_github_tags
+    list_github_tags
 }
 
 download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+    local version filename url
+    version="$1"
+    filename="$2"
 
-	url="$GH_REPO/releases/download/v${version}/zksolc-$OS-v${version}"
+    url="$GH_REPO/releases/download/v${version}/zksolc-$OS-v${version}"
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+    echo "* Downloading $TOOL_NAME release $version..."
+    curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="$3"
+    local install_type="$1"
+    local version="$2"
+    local install_path="$3"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+    if [ "$install_type" != "version" ]; then
+        fail "asdf-$TOOL_NAME supports release installs only"
+    fi
 
-	(
-		mkdir -p "$install_path/bin"
-		local tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		local bin_file="$install_path/bin/$tool_cmd"
+    (
+        mkdir -p "$install_path/bin"
+        local tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+        local bin_file="$install_path/bin/$tool_cmd"
 
-		download_release "$version" "$bin_file"
-		chmod a+x "$install_path/bin/$tool_cmd" || fail "Could not chmod +x $install_path/bin/$tool_cmd."
+        download_release "$version" "$bin_file"
+        chmod a+x "$install_path/bin/$tool_cmd" || fail "Could not chmod +x $install_path/bin/$tool_cmd."
 
-		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+        test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+        echo "$TOOL_NAME $version installation was successful!"
+    ) || (
+        rm -rf "$install_path"
+        fail "An error occurred while installing $TOOL_NAME $version."
+    )
 }
